499deb045bb14aaa48912c8dd813ae6ff8fac618
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-09-29 10:38:45 +0200

    new form setup

--- client/src/App.js
+++ client/src/App.js
@@ -55,22 +55,14 @@ function App() {
   }
 
   return (
-    <div className="App container" style={{ marginTop: 20 }}>
-        <h3>Create New Article</h3>
-      <h3>Add Artwalk</h3>
+    <div className="App container" style={{ marginTop: 20 }}>      
+    <h3>Add Artwalk</h3>
       <form className="formPost" onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Title of the article: </label><input id="Title" onChange={handleChange} name="title" value={artwalk.title} /></div>
-            <div className="form-group">
-            <label>Article's description: </label><input id="Description" onChange={handleChange} name="description" value={artwalk.description}/></div>
-            <div className="form-group">
-            <input
-              type="submit"
-              value="Create artwalk"
-              className="btn btn-primary"
-            />
-          </div>
-        <button onClick={addArtwalk}>Add artwalk</button>
+            <label>Title of the Artwalk: </label>
+            <input id="Title" onChange={handleChange} name="title" value={artwalk.title}></input>
+            <label>Description of the Artwalk: </label>
+            <input id="Description" onChange={handleChange} name="description" value={artwalk.description}></input>
+        <button className="btn btn-primary" onClick={addArtwalk}>Add artwalk</button>
       </form>
 
       {artwalks.map(artwalk => {
@@ -79,7 +71,7 @@ function App() {
             <h1>Artwalks</h1>
             <h1 className="h1Title">{artwalk.title}</h1>
             <p className="pDescription">{artwalk.description}</p>
-            <button class="btn btn-primary" onClick={() => deleteArtwalk(artwalk._id)}>Delete</button>
+            <button onClick={() => deleteArtwalk(artwalk._id)}>Delete</button>
           </div>
         ) 
       })}
