a76e2075761e63f142c4191663e48dfa01c00b8a
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-10-29 09:54:34 +0200

    First mini deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
-//import logo from './logo.svg';
 import axios from "axios";
-//import 'bootstrap/dist/css/bootstrap.css';
+
+import Logo from "./logo/Logo";
 
 import './App.css';
 
@@ -76,6 +76,7 @@ function App() {
           </div>
         ) 
       })}
+      <Logo />
     </div>
   );
 }
